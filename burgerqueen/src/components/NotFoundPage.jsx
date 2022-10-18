6fbebf06016d559a80b4baabd7c01b73905385d7
Yadira Garcia <leyadve@gmail.com>
2022-10-17 21:38:57 -0500

    components NotFoundPage completed

--- burgerqueen/src/components/NotFoundPage.jsx
+++ burgerqueen/src/components/NotFoundPage.jsx
@@ -1,8 +1,13 @@
+import ups from "../imagens/ups.png";
+import '../NotFoundPage.css'
+
 
 export default function NotFoundPage() {
   return (
-    <div>
-      Error 404 Pagina no encontrada
+    <div className="notFound" >
+      <img src={ups} alt="error 404" className="imgError"/>
+      <p className="textError">Error 404 Pagina no encontrada</p>
+      
     </div>
   )
 }
